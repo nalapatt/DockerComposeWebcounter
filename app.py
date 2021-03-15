4c8457906ab15092ac44a6e16416fd05a9dc3140
nalapatt <70668968+nalapatt@users.noreply.github.com>
2021-03-15 13:51:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,37 @@
-import time
-import redis
-
 from flask import Flask
 app = Flask(__name__)
 
-cache = redis.Redis(host='localhost', port=6379)
 
-def my_function():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
+@app.route('/')
 def hello():
-    count = my_function()
-    return 'Hello world! I have been seen () times.\n'.format(count)
+    return "Hello World!"
 
-if app == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+if __name__ == '__main__':
+    app.run()
+
+
+#import time
+#import redis
+
+#from flask import Flask
+#app = Flask(__name__)
+
+#cache = redis.Redis(host='localhost', port=6379)
+
+#def my_function():
+#    retries = 5
+#    while True:
+#        try:
+#            return cache.incr('hits')
+#        except redis.exceptions.ConnectionError as exc:
+#            if retries == 0:
+#                raise exc
+#            retries -= 1
+#            time.sleep(0.5)
+
+#def hello():
+#    count = my_function()
+#    return 'Hello world! I have been seen () times.\n'.format(count)
+
+#if app == "__main__":
+#    app.run(host="0.0.0.0",debug=True)

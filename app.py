5985d4e9b508a40b50f56e8b216c26fba8a46929
nalapatt <70668968+nalapatt@users.noreply.github.com>
2021-03-15 14:16:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
+
 from flask import Flask
+from redis import Redis
+
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-if __name__ == '__main__':
-    app.run()
-
+redis = Redis(host='redis', port=6379)
 
 #import time
 #import redis
